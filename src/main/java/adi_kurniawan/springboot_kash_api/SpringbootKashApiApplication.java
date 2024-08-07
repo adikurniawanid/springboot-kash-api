b17855703c18cb0ff24cf0c25a20535e541d904c
Adi Kurniawan <adikr@hanabank.co.id>
2024-08-07 17:00:24 +0700

    initial commit

---
+++ src/main/java/adi_kurniawan/springboot_kash_api/SpringbootKashApiApplication.java
@@ -0,0 +1,13 @@
+package adi_kurniawan.springboot_kash_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootKashApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootKashApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/adi_kurniawan/springboot_kash_api/SpringbootKashApiApplicationTests.java
@@ -0,0 +1,13 @@
+package adi_kurniawan.springboot_kash_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootKashApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
